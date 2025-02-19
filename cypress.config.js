ebe58b99a2130a72ccc15ae27af1b265dea5f087
@renan.matos <renanpacheco.delta@gmail.com>
2025-02-18 21:14:37 -0300

    commit ajuste configs e report

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,14 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  reporter: "mochawesome",
+
+  reporterOptions: {
+    reportDir: "cypress/reports",
+    overwrite: true,
+    html: true,
+    json: false,
+  },
   e2e: {
     baseUrl: "https://www.narwalsistemas.com.br",
     viewportWidth: 1280,
